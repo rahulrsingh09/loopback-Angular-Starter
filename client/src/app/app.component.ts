8a3cd171aaf685cabcb5c4dacc3acb79e3c098ee
Rahul Singh <rahulrsingh09@gmail.com>
2017-12-12 13:38:51 +0530

    Angular

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { LoopBackConfig } from './shared/sdk/lb.config';
 import { Component } from '@angular/core';
+import { Angular_LoopbackApi } from './shared/sdk/index';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  greet : string ;
+  constructor(private al : Angular_LoopbackApi){
+    //Just note that this will need to be done to new components that you create if you want to give it access to our loopback app.
+    LoopBackConfig.setBaseURL("http://127.0.0.1:3000");
+    LoopBackConfig.setApiVersion('api');
+  }
+
+  submit(value){
+    this.al.greet(value).subscribe(data => this.greet = data.greeting);
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
+import {MatInputModule} from '@angular/material/input'
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatButtonModule, MatButton} from '@angular/material/button';
+
 
 import { AppComponent } from './app.component';
+import { SDKBrowserModule } from './shared/sdk/index';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    FormsModule,
+    HttpModule,
+    SDKBrowserModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
