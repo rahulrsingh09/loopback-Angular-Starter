c6b2ae3f02b2678611db6b1ab438f91442fb38c0
Rahul Singh <rahulrsingh09@gmail.com>
2017-12-12 12:07:15 +0530

    Loopback and Angular Configured

---
+++ common/models/angular_loopback.js
@@ -0,0 +1,10 @@
+"user strict";
+
+module.exports = function(Angular) {
+  Angular.greet = function(msg, cb) {
+    process.nextTick(function() {
+      msg = msg || 'hello';
+      cb(null, 'Angular and Loopback is now Configured For ' + msg);
+    });
+  };
+};
