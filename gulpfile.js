19867bb99efbf2b887923b8c37d24455eec74d77
Rahul Singh <rahulrsingh09@gmail.com>
2017-12-14 18:53:52 +0530

    automated build gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,53 @@
 'use strict';
 
 var gulp = require('gulp');
-var spawn = require('child_process').spawn;
+var {spawn} = require('child_process');
 var node;
+var browserSync = require('browser-sync');
+var nodemon = require('gulp-nodemon');
+var gutil = require('gulp-util');
+var run = require('gulp-run');
+var runSequence = require('run-sequence');
+
+/* Run the npm script npm run buildLsdk using gulp */
+gulp.task('sdk', function() {
+  // process.chdir('server');
+  spawn('./node_modules/.bin/lb-sdk', ['server/server.js', './client/src/app/shared/sdk', '-q'], {stdio: 'inherit'});
+});
+
+/* Change detection on Angular app and then do npm build */
+var exec = require('child_process').exec;
+
+gulp.task('angular', function(cb) {
+  process.chdir('client');
+  exec('ng build -w', function(err, stdout, stderr) {
+  });
+});
 
 /**
-* $ gulp server
-* description: launch the server. If there's a server already running, kill it.
+* $ gulp
+* description: start the development environment
 */
+gulp.task('default', function() {
+  runSequence('sdk', 'browser-sync', 'server', 'angular', function() {
+    gulp.watch(['./common/models/*.js'], ['server']);
+  });
+});
+
+// clean up if an error goes unhandled.
+process.on('exit', function() {
+  if (node) node.kill();
+});
+
+gulp.task('browser-sync', function() {
+  browserSync.init(null, {
+    proxy: 'http://localhost:3000/home',
+    browser: 'google chrome',
+    port: 7000,
+  });
+  gulp.watch(['client/src/app/*.ts'], browserSync.reload);
+});
+
 gulp.task('server', function() {
   if (node) node.kill();
   node = spawn('node', ['server/server.js'], {stdio: 'inherit'});
@@ -16,24 +56,4 @@ gulp.task('server', function() {
       gulp.log('Error detected, waiting for changes...');
     }
   });
-});
-
-gulp.task('sdk', function() {
-  spawn('./node_modules/.bin/lb-sdk', ['server/server.js', './client/src/app/shared/sdk', '-q'], {stdio: 'inherit'});
-});
-
-/**
-* $ gulp
-* description: start the development environment
-*/
-gulp.task('default', ['sdk', 'server'], function() {
-  gulp.watch(['./common/models/*.js'], ['server']);
-  // Need to watch for sass changes too? Just add another watch call!
-  // no more messing around with grunt-concurrent or the like. Gulp is
-  // async by default.
-});
-
-// clean up if an error goes unhandled.
-process.on('exit', function() {
-  if (node) node.kill();
 });
